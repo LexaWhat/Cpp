6c8bd8e9d296c3c6b7c4a35598cf9e9240482e5e
LexaWhat <udiejfkskfid@gmail.com>
2025-04-04 21:37:27 +0500

    Base Comfort bugfix

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <string>
 
-int print(const std::string& str) {
+void print(const std::string& str) {
     std::cout << str;
-    return 0;
 }
 
-int println(const std::string& str) {
+void println(const std::string& str) {
     std::cout << str << std::endl;
-    return 0;
 }
 
 int main() {
