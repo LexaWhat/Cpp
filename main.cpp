4872faeadf471b879884d8777679943f2b9116c8
LexaWhat <udiejfkskfid@gmail.com>
2025-04-04 21:30:59 +0500

    Base Comfort

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
-int main() {
+#include <iostream>
+#include <string>
 
+int print(const std::string& str) {
+    std::cout << str;
+    return 0;
+}
+
+int println(const std::string& str) {
+    std::cout << str << std::endl;
+    return 0;
+}
+
+int main() {
+    println("Hello World!");
+    println("Hello World!");
 }
